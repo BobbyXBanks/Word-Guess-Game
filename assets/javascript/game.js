ba0c31e252fdec8befa855ab1aec634b1f5b3e4a
bobbyxbanks <b_x_m@live.com>
2018-06-27 18:58:38 -0700

    created folder structure and base files

---
+++ assets/javascript/game.js
@@ -0,0 +1,1 @@
+// * In the `javascript` folder, make a file called `game.js`. Use the `src` attribute of the `script` tag to link to this file, rather than embedding the code directly in your HTML document.
