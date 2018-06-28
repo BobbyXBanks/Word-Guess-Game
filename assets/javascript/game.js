ef25d46d1e3ebbc651c48ec60203aa29e3333ae5
bobbyxbanks <b_x_m@live.com>
2018-06-28 16:30:40 -0700

    upload assignment

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,183 @@
-// * In the `javascript` folder, make a file called `game.js`. Use the `src` attribute of the `script` tag to link to this file, rather than embedding the code directly in your HTML document.
+// I couldn't figure out how to have two word answers with a space between the words like russian blue so I had to delete the space for it to work
+// also could not get the "you lose" or "you win" to display even though my wins and losses are being counted
+
+var wins = 0;
+var losses = 0;
+
+var maxErrors = 9;
+
+var wordDisplayLettersElement = document.getElementById("word-display-letters");
+var guessedLettersElement = document.getElementById("guessed-letters");
+var errorCountElement = document.getElementById("error-count");
+var winCountElement = document.getElementById("win-count");
+var lossCountElement = document.getElementById("loss-count");
+
+var blinkElements = document.getElementsByClassName("blinking");
+var alertLineElements = document.getElementsByClassName("alert-line");
+
+var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+var pressAnyKeyToStart = [
+	"press any key to start"
+];
+var pressAnyKeyToReset = [
+	"press any key to play again"
+];
+
+var youWin = [
+	"you win!"
+];
+var youLose = [
+	"you lose!"
+];
+var emptyAlert = [
+	"                                           ",
+	"                                           ",
+	"                                           ",
+	"                                           ",
+	"                                           "
+];
+
+var game = new Hangman();
+
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+
+	if (!game.gameOver) {
+		if (validGuesses.includes(userGuess) && !game.guessedLetters.includes(userGuess)) {
+			game.checkGuess(userGuess);
+		}
+	} else {
+		game = new Hangman();
+		game.updatePageData();
+	}
+}
+
+window.setInterval(function() {
+	if (blinkElements.length > 0) {
+		if (game.guessedLetters.length === 0 || game.gameOver) {
+			if (blinkElements[0].style.opacity === "1") {
+				for (var i = 0; i < blinkElements.length; i++) {
+					blinkElements[i].style.opacity = "0";
+				}
+			} else {
+				for (var i = 0; i < blinkElements.length; i++) {
+					blinkElements[i].style.opacity = "1";
+				}
+			}
+		} else {
+			for (var i = 0; i < blinkElements.length; i++) {
+				blinkElements[i].style.opacity = "0";
+			}
+		}
+	}
+}, 750);
+
+function Hangman() {
+	this.wordList = [
+		"siamese",
+        "persian",
+        "mainecoon",
+        "bengal",
+        "americanshorthair",
+        "sphynx",
+        "burmese",
+        "siberian",
+        "britishshorthair",
+        "abyssinian",
+        "manx",
+        "chartreux",
+        "bombay",
+        "russianblue",
+        "himalayan",
+        "balinese",
+        "ocicat",
+        "alley"
+	]
+
+	this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+	this.guessedLetters = [];
+	this.errors = 0;
+	this.visibleLetters = [];
+	this.gameOver = false;
+	this.alertLines = emptyAlert;
+	for (var i = 0; i < this.word.length; i++) {
+		this.visibleLetters[i] = (false);
+	}
+}
+
+Hangman.prototype.checkGuess = function(char) {
+	this.guessedLetters.push(char);
+
+	var isInWord = false;
+	for (var i = 0; i < this.word.length; i++) {
+		if (this.word.charAt(i) === char) {
+			isInWord = true;
+			this.visibleLetters[i] = true;
+		}
+	}
+	if (!isInWord) {
+		this.errors++;
+	}
+
+	if (this.errors >= maxErrors) {
+		losses++;
+		this.alertLines = youLose;
+		this.gameOver = true;
+	}
+
+	if (!this.visibleLetters.includes(false)) {
+		wins++;
+		this.alertLines = youWin;
+		this.gameOver = true;
+	}
+
+	game.updatePageData();
+};
+
+Hangman.prototype.updatePageData = function() {
+	var tempString = "";
+	for (var i = 0; i < this.visibleLetters.length; i++) {
+		tempString += ((this.visibleLetters[i] || this.gameOver) ? this.word.charAt(i).toUpperCase() : "_");
+		if (i < (this.visibleLetters.length - 1)) tempString += " ";
+	}
+	wordDisplayLettersElement.textContent = tempString;
+
+	tempString = "";
+	for (var i = 0; i < this.guessedLetters.length; i++) {
+		tempString += (this.guessedLetters[i].toUpperCase());
+		if (i < (this.guessedLetters.length - 1)) tempString += " ";
+	}
+	for (var i = tempString.length; i < 51; i++) {
+		tempString += " ";
+	}
+	guessedLettersElement.textContent = tempString;
+
+	tempString = this.errors + " / " + maxErrors;
+	for (var i = tempString.length; i < 32; i++) {
+		tempString += " ";
+	}
+	errorCountElement.textContent = tempString;
+
+	tempString = wins + "";
+	for (var i = tempString.length; i < 45; i++) {
+		tempString += " ";
+	}
+	winCountElement.textContent = tempString;
+
+	tempString = losses + "";
+	for (var i = tempString.length; i < 43; i++) {
+		tempString += " ";
+	}
+	lossCountElement.textContent = tempString;
+
+	for (var i = 0; i < blinkElements.length; i++) {
+		blinkElements[i].textContent = (this.gameOver ? pressAnyKeyToReset[i] : pressAnyKeyToStart[i]);
+	}
+
+	for (var i = 0; i < alertLineElements.length; i++) {
+		alertLineElements[i].textContent = (this.alertLines[i]);
+	}
+}
+
+game.updatePageData();
